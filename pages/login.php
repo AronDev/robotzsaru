23b04407302e34239dfafbc66b03825570727ee2
Áron Kovács <zipike02@gmail.com>
2019-07-13 23:19:01 +0200

    login.js hozzáadva

--- pages/login.php
+++ pages/login.php
@@ -15,38 +15,7 @@
             <div id='loginInfo'>&nbsp</div><br />
         </div>
     </body>
-    <script>
-    $('body').on('click', '#loginButton', function(e) {
-            e.preventDefault();
-            var badgeNum = $('#badgeNum').val();
-            var password = $('#password').val();
-
-            var loginInfo = $('#loginInfo');
-
-            if($.trim(badgeNum) !== '') {
-                if($.trim(password) !== '') {
-                    $.ajax({
-                        type: 'POST',
-                        url: 'includes/login.inc.php',
-                        data: { badgeNum : badgeNum, password : password },
-                        success: function(response) {
-                            if(response == "reload")
-                                location.reload();
-                            else
-                                loginInfo.html(response);
-                        },
-                        error: function (response) {
-                            console.log(response);
-                        }
-                    });
-                } else {
-                    loginInfo.html('Hiányos jelszó!');
-                }
-            } else {
-                loginInfo.html('Hiányos jelvényszám!');
-            }
-        });
-    </script>
+    <script src="js/login.js"></script>
 </html>
 <?php
 //
