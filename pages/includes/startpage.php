9dbaff2d137582d349f20e6ab35c0b94611537f5
Áron Kovács <zipike02@gmail.com>
2019-07-14 01:59:06 +0200

    Főoldal bővítés

--- pages/includes/startpage.php
+++ pages/includes/startpage.php
@@ -1,9 +1,27 @@
 <div id="startpage-widgets">
-    <div class="startpage-widget">
+    <div onclick="window.location.href = 'index.php?p=activities';" class="startpage-widget">
         <div class='title'>
             Tevékenységek
             <div class=description>
                 Előállítások, helyszíni bírságok, lefoglalások, stb..
+            </div>
+        </div>
+    </div>
+
+    <div onclick="window.location.href = 'index.php?p=files';" class="startpage-widget">
+        <div class='title'>
+            Akták
+            <div class=description>
+                Nyomozati akták
+            </div>
+        </div>
+    </div>
+
+    <div onclick="window.location.href = 'index.php?p=reports';" class="startpage-widget">
+        <div class='title'>
+            Jegyzőkönyvek
+            <div class=description>
+                Kihallgatási jegyzőkönyvek, helyszíni jegyzőkönyvek, stb..
             </div>
         </div>
     </div>
