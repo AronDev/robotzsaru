0f3c9232b7e4345012758526a862c1410ec78e9f
Áron Kovács <zipike02@gmail.com>
2019-07-13 22:35:39 +0200

    jquery hozzáadva register.php-hez iletve név megadás

--- pages/register.php
+++ pages/register.php
@@ -3,6 +3,8 @@
         <title>Robotzsaru - Regisztrálás</title>
         <meta charset="utf-8">
 
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
         <link rel="stylesheet" type="text/css" href="../styles/main.min.css?<?php echo time(); ?>"/>
 
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
@@ -16,6 +18,7 @@
             <h4>Regisztrálás</h4>
             </div><br />
             <input type="text" id="badgeNum" placeholder="Jelvényszám"><br /><br />
+            <input type="text" id="playerName" placeholder="Név"><br /><br />
             <input type="password" id="password" placeholder="Jelszó"><br /><br />
             <input type="password" id="password2" placeholder="Jelszó mégegyszer"><br /><br />
             <button id="registerButton"><i class='fas fa-user-plus login-icon'></i>Regisztrálás</button><br /><br />
@@ -27,33 +30,38 @@
     $('body').on('click', '#loginButton', function(e) {
             e.preventDefault();
             var badgeNum = $('#badgeNum').val();
+            var playerName = $('#playerName').val();
             var password = $('#password').val();
             var password2 = $('#password2').val();
 
             var registerInfo = $('#loginInfo');
 
             if($.trim(badgeNum) !== '') {
-                if($.trim(password) !== '' || $.trim(password2) !== '') {
-                    if(password === pasword2) {
-                        $.ajax({
-                            type: 'POST',
-                            url: 'includes/register.inc.php',
-                            data: { badgeNum : badgeNum, password : password },
-                            success: function(response) {
-                                if(response == "index")
-                                    window.location.href = '../index.php';
-                                else
-                                    registerInfo.html(response);
-                            },
-                            error: function (response) {
-                                console.log(response);
-                            }
-                        });
+                if($.trim(playerName) !== '') {
+                    if($.trim(password) !== '' || $.trim(password2) !== '') {
+                        if(password === pasword2) {
+                            $.ajax({
+                                type: 'POST',
+                                url: 'includes/register.inc.php',
+                                data: { badgeNum : badgeNum, password : password },
+                                success: function(response) {
+                                    if(response == "index")
+                                        window.location.href = '../index.php';
+                                    else
+                                        registerInfo.html(response);
+                                },
+                                error: function (response) {
+                                    console.log(response);
+                                }
+                            });
+                        } else {
+                            registerInfo.html('A két jelszó nem megegyező!');
+                        }
                     } else {
-                        registerInfo.html('A két jelszó nem megegyező!');
+                        registerInfo.html('Hiányos jelszó!');
                     }
                 } else {
-                    registerInfo.html('Hiányos jelszó!');
+                    registerInfo.html('Hiányos név!');
                 }
             } else {
                 registerInfo.html('Hiányos jelvényszám!');
