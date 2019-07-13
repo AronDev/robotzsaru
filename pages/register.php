c32cbd4d9e721d7dfc3583d0f0dd5a3b9570f253
Áron Kovács <zipike02@gmail.com>
2019-07-13 23:13:03 +0200

    teszt

--- pages/register.php
+++ pages/register.php
@@ -3,8 +3,6 @@
         <title>Robotzsaru - Regisztrálás</title>
         <meta charset="utf-8">
 
-        <script src="../js/register.js"></script>
-
         <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
         <link rel="stylesheet" type="text/css" href="../styles/main.min.css?<?php echo time(); ?>"/>
@@ -12,6 +10,8 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
         <link rel="shortcut icon" type="image/png" href=""/>
+
+        <script src="../js/register.js"></script>
     </head>
     <body>
         <div id="login-box">
