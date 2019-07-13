6f7138f49f31080101186209cf112fbfa730f5be
Áron Kovács <zipike02@gmail.com>
2019-07-13 23:26:27 +0200

    debug

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -8,15 +8,15 @@ $password = mysqli_real_escape_string($mysql_id, $_POST["password"]);
 
 $pwHash = hash('sha512', $password); // Bevitt jelszó dekódolása SHA512-vel
 
-if($result = mysqli_query($mysql_id, "SELECT badge_number FROM users WHERE badge_number='" . $badgeNum . "'")) {
+if($result = mysqli_query($mysql_id, "SELECT badge_number FROM users WHERE badge_number='" . $badgeNum . "' OR playername='" . $playerName . "'")) {
     if(mysqli_num_rows($result) == 0) {
         $query = "INSERT INTO users (`badge_number`, `playername`, `password`) VALUES ('" . $badgeNum . "', '" . $playerName . "', '" . $pwHash . "')";
 
         if(mysqli_query($mysql_id, $query)) {
             echo "Felhasználód létrehozva!\n Várj türelemmel míg elfogadják azt!";
-        } else echo "Hiba történt!";
-    } else echo "Már létezik felhasználó ezzel a jelvényszámmal!";
-} else echo "Hiba történt!";
+        } else echo "Hiba történt!2";
+    } else echo "Már létezik felhasználó ezzel a jelvényszámmal vagy névvel!";
+} else echo "Hiba történt!1";
 
 // POST adatok törlése
 unset($_POST);
