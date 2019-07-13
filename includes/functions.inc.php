bc190589805f932378e3e3107f929beae9595c15
Áron Kovács <zipike02@gmail.com>
2019-07-14 01:01:36 +0200

    a

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1,11 +1,4 @@
 <?php
-function getUserData($badgeNum, $column) {
-    require("connect.inc.php");
-    $result = mysqli_query($mysql_id, "SELECT ".$column." FROM users WHERE badge_number='" . $badgeNum . "'");
-    $row = mysqli_fetch_assoc($result);
-    return $row['".$column."'];
-}
-
 function getUserName($badgeNum) {
     require("connect.inc.php");
     $result = mysqli_query($mysql_id, "SELECT playername FROM users WHERE badge_number='" . $badgeNum . "'");
