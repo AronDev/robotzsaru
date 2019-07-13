40da429f172bcdfc0a77c604743823683744e4cb
Áron Kovács <zipike02@gmail.com>
2019-07-13 17:44:45 +0200

    MySQLL csatlakozás fix

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,8 +1,8 @@
 <?php
 define("HOSTNAME","localhost");
-define("USERNAME","aron");
-define("PASSWORD","ArOn2002Sunwell1");
-define("DATABASE","insiderpg");
+define("USERNAME","robotzsaru");
+define("PASSWORD","r1o2b3o4t5z6s7a8r9u0");
+define("DATABASE","robotzsaru");
 $mysql_id = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
 if (mysqli_connect_errno()) echo mysqli_connect_errno() . ": " . mysqli_connect_error();
 mysqli_query($sql_id,"SET NAMES UTF8");
