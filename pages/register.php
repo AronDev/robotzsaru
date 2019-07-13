2d44ad82be27f088dad6e5a376215269f0f43a99
Áron Kovács <zipike02@gmail.com>
2019-07-13 22:37:25 +0200

    Icon csere, register button fix

--- pages/register.php
+++ pages/register.php
@@ -9,7 +9,7 @@
 
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-        <link rel="shortcut icon" type="image/png" href="https://banner2.kisspng.com/20180320/dze/kisspng-police-officer-computer-icons-iconfinder-police-icon-hd-5ab16a2db522d3.1179238215215764937419.jpg"/>
+        <link rel="shortcut icon" type="image/png" href="http://www.sajoszentpeter.hu/_new/wp-content/uploads/2018/02/rendorseg-logo.jpg"/>
     </head>
     <body>
         <div id="login-box">
@@ -27,7 +27,7 @@
         </div>
     </body>
     <script>
-    $('body').on('click', '#loginButton', function(e) {
+    $('body').on('click', '#registerButton', function(e) {
             e.preventDefault();
             var badgeNum = $('#badgeNum').val();
             var playerName = $('#playerName').val();
