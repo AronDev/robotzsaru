62d6a4e0d0067fed5c675d6d69f98bb523f8472d
Áron Kovács <zipike02@gmail.com>
2019-07-14 01:53:34 +0200

    a

--- pages/includes/startpage.php
+++ pages/includes/startpage.php
@@ -2,10 +2,9 @@
     <div class="startpage-widget">
         <div class='title'>
             Tevékenységek
-        </div>
-        <i class="far fa-clipboard icon"></i>
-        <div class=description>
-            Előállítások, helyszíni bírságok, lefoglalások, stb..
+            <div class=description>
+                Előállítások, helyszíni bírságok, lefoglalások, stb..
+            </div>
         </div>
     </div>
 </div>
