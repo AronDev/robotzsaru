639360249fe928cc59d142bd2fff31e01c5b6083
Áron Kovács <zipike02@gmail.com>
2019-07-13 22:58:29 +0200

    Fix

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -8,10 +8,14 @@ $password = mysqli_real_escape_string($mysql_id, $_POST["password"]);
 
 $pwHash = hash('sha512', $password); // Bevitt jelszó dekódolása SHA512-vel
 
-$query = "INSERT INTO users ('badge_number', 'playername', 'password') VALUES ('" . $badgeNum . "', '" . $playerName . "', '" . $pwHash . "')";
+if($result = mysqli_query($mysql_id, "SELECT badge_number FROM users WHERE badge_number='" . $badgeNum . "'")) {
+    if(mysqli_num_rows($result) == 0) {
+        $query = "INSERT INTO users ('badge_number', 'playername', 'password') VALUES ('" . $badgeNum . "', '" . $playerName . "', '" . $pwHash . "')";
 
-if(mysqli_query($mysql_id, $query)) {
-    echo "Felhasználód létrehozva!\n Várj türelemmel míg elfogadják azt!";
+        if(mysqli_query($mysql_id, $query)) {
+            echo "Felhasználód létrehozva!\n Várj türelemmel míg elfogadják azt!";
+        } else echo "Hiba történt!";
+    } else echo "Már létezik felhasználó ezzel a jelvényszámmal!";
 } else echo "Hiba történt!";
 
 // POST adatok törlése
