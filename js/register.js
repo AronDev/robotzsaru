4fa26335da4eddd7620dad710cef628d7e1b9bc2
Áron Kovács <zipike02@gmail.com>
2019-07-13 23:17:11 +0200

    teszt

--- js/register.js
+++ js/register.js
@@ -1,36 +1,36 @@
 $('body').on('click', '#registerButton', function(e) {
-        e.preventDefault();
-        var badgeNum = $('#badgeNum').val();
-        var playerName = $('#playerName').val();
-        var password = $('#password').val();
-        var password2 = $('#password2').val();
+    e.preventDefault();
+    var badgeNum = $('#badgeNum').val();
+    var playerName = $('#playerName').val();
+    var password = $('#password').val();
+    var password2 = $('#password2').val();
 
-        var registerInfo = $('#registerInfo');
+    var registerInfo = $('#registerInfo');
 
-        if($.trim(badgeNum) !== '') {
-            if(!isNaN(badgeNum)) {
-                if($.trim(playerName) !== '') {
-                    if(playerName.indexOf('_') !== -1) {
-                        if($.trim(password) !== '' || $.trim(password2) !== '') {
-                            if(password === password2) {
-                                $.ajax({
-                                    type: 'POST',
-                                    url: '../includes/register.inc.php',
-                                    data: { badgeNum : badgeNum, password : password },
-                                    success: function(response) {
-                                        if(response == "index")
-                                            window.location.href = '../index.php';
-                                        else
-                                            registerInfo.html(response);
-                                    },
-                                    error: function (response) {
-                                        console.log(response);
-                                    }
-                                });
-                            } else egisterInfo.html('A két jelszó nem megegyező!');
-                        } else registerInfo.html('Hiányos jelszó!');
-                    } else registerInfo.html('A névnek tartalmaznia kell alsóvonalat!');
-                } else registerInfo.html('Hiányos név!');
-            } else registerInfo.html('Hibás jelvényszám!');
-        } else registerInfo.html('Hiányos jelvényszám!');
-    });
+    if($.trim(badgeNum) !== '') {
+        if(!isNaN(badgeNum)) {
+            if($.trim(playerName) !== '') {
+                if(playerName.indexOf('_') !== -1) {
+                    if($.trim(password) !== '' || $.trim(password2) !== '') {
+                        if(password === password2) {
+                            $.ajax({
+                                type: 'POST',
+                                url: '../includes/register.inc.php',
+                                data: { badgeNum : badgeNum, password : password },
+                                success: function(response) {
+                                    if(response == "index")
+                                        window.location.href = '../index.php';
+                                    else
+                                        registerInfo.html(response);
+                                },
+                                error: function (response) {
+                                    console.log(response);
+                                }
+                            });
+                        } else egisterInfo.html('A két jelszó nem megegyező!');
+                    } else registerInfo.html('Hiányos jelszó!');
+                } else registerInfo.html('A névnek tartalmaznia kell alsóvonalat!');
+            } else registerInfo.html('Hiányos név!');
+        } else registerInfo.html('Hibás jelvényszám!');
+    } else registerInfo.html('Hiányos jelvényszám!');
+});
