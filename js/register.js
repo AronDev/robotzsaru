55a70b11b74f04bed2cbf1ba74f1ef05a50f640e
Áron Kovács <zipike02@gmail.com>
2019-07-14 00:05:26 +0200

    asd

--- js/register.js
+++ js/register.js
@@ -1,7 +1,8 @@
 $('body').on('click', '#registerButton', function(e) {
     e.preventDefault();
     var badgeNum = $('#badgeNum').val();
-    var playerName = $('#playerName').value();
+    var playerName = $("#playerName")[0].value = ($("#playerName")[0].value;
+    //var playerName = $('#playerName').val();
     var password = $('#password').val();
     var password2 = $('#password2').val();
 
