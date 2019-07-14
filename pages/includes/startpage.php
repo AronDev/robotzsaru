f98f20b1e4d50488e51c6dc2c3f8e2b1952d654b
Áron Kovács <zipike02@gmail.com>
2019-07-14 02:52:24 +0200

    asd

--- pages/includes/startpage.php
+++ pages/includes/startpage.php
@@ -2,11 +2,11 @@
     <div onclick="window.location.href = 'index.php?p=activities';" class="startpage-widget">
         <div class='title'>
             Tevékenységek
-            <div class=description>
+            <div class='description'>
                 Előállítások, helyszíni bírságok, lefoglalások, stb..
             </div>
         </div>
-        <div>
+        <div class='description'>
             <?php
             $result = mysqli_query($mysql_id, "SELECT title, users.playername FROM activities INNER JOIN users ON users.badge_number=activities.user ORDER BY timestamp DESC LIMIT 1");
             if(mysqli_num_rows($result) > 0) {
@@ -20,7 +20,7 @@
     <div onclick="window.location.href = 'index.php?p=files';" class="startpage-widget">
         <div class='title'>
             Akták
-            <div class=description>
+            <div class='description'>
                 Nyomozati akták
             </div>
         </div>
@@ -29,7 +29,7 @@
     <div onclick="window.location.href = 'index.php?p=reports';" class="startpage-widget">
         <div class='title'>
             Jegyzőkönyvek
-            <div class=description>
+            <div class='description'>
                 Kihallgatási jegyzőkönyvek, helyszíni jegyzőkönyvek, stb..
             </div>
         </div>
