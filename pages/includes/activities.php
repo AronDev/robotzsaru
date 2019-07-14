128681edf87d79b205a9d9537dc28d88a66fed1e
Áron Kovács <zipike02@gmail.com>
2019-07-14 02:41:00 +0200

    asd

--- pages/includes/activities.php
+++ pages/includes/activities.php
@@ -15,12 +15,28 @@
                     echo $row['playername'] . " - " . $row['timestamp'];
                     echo "</div>";
                     echo "</div>";
-                    echo "<div class='msg'>";
+                    echo "<div class='msg dropdown'>";
                     echo substr($row['text'], 0, 30) . "...";
                     echo "</div>";
                 echo "</div>";
             }
         } else echo "Nincsenek tevékenységek!";
         ?>
+        <script>
+        var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+</script>
     </body>
 </html>
