b8606caec9be0719c6cfbb6b34e2795719ff5f44
Áron Kovács <zipike02@gmail.com>
2019-07-14 02:41:14 +0200

    asd

--- pages/includes/activities.php
+++ pages/includes/activities.php
@@ -23,20 +23,20 @@
         } else echo "Nincsenek tevékenységek!";
         ?>
         <script>
-        var acc = document.getElementsByClassName("accordion");
-var i;
+        var acc = document.getElementsByClassName("dropwdown");
+        var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-</script>
+        for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight){
+              panel.style.maxHeight = null;
+            } else {
+              panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+          });
+        }
+        </script>
     </body>
 </html>
