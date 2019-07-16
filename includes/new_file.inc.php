e21c3c8884e45d8b82d1c1b3b70700a5eea84d6f
Áron Kovács <zipike02@gmail.com>
2019-07-16 23:09:29 +0200

    123

--- includes/new_file.inc.php
+++ includes/new_file.inc.php
@@ -9,7 +9,8 @@ $text = mysqli_real_escape_string($mysql_id, $_POST["text"]);
 $badgenum = $_SESSION['badge_number'];
 
 if($result = mysqli_query($mysql_id, "INSERT INTO files (`file_name`, `title`, `author`, `text`) VALUES ('$filename', '$title', '$badgenum', '$text')")) {
-    echo mysqli_insert_id();
+    $insertID = mysqli_insert_id($result);
+    echo $insertID;
 } else echo "Hiba történt!";
 
 // POST adatok törlése
