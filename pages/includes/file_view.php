dec1e2cd27a0154e52f0cde5d41feaf97985aa4d
Áron Kovács <zipike02@gmail.com>
2019-07-16 18:08:18 +0200

    width fix

--- pages/includes/file_view.php
+++ pages/includes/file_view.php
@@ -1,11 +1,13 @@
 <?php
 if($result = mysqli_query($mysql_id, "SELECT * FROM files WHERE dbid='$dbid'")) {
     $row = mysqli_fetch_assoc($result);
+    echo "<did id='files-content'>";
     echo "<table id='files'>";
         echo "<tr>";
             echo "<td><b>Akta sorszáma</b></td>";
             echo "<td>" . $row['file_name'] . "</td>";
         echo "</tr>";
     echo "</table>";
+    echo "</div>";
 } else echo "<h1>Hiba történt!</h1>";
 ?>
