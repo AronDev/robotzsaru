1d04fb23f57c0b400ca09c4fbcfb490ae4f64dca
Áron Kovács <zipike02@gmail.com>
2019-07-16 18:08:51 +0200

    1

--- pages/includes/file_view.php
+++ pages/includes/file_view.php
@@ -1,7 +1,7 @@
 <?php
 if($result = mysqli_query($mysql_id, "SELECT * FROM files WHERE dbid='$dbid'")) {
     $row = mysqli_fetch_assoc($result);
-    echo "<did id='files-content'>";
+    echo "<div id='files-content'>";
     echo "<table id='files'>";
         echo "<tr>";
             echo "<td><b>Akta sorszáma</b></td>";
