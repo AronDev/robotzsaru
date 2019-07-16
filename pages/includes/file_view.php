427c5fffec7d87f7aa08a33a8f36a07c90ffb4b5
Áron Kovács <zipike02@gmail.com>
2019-07-16 19:11:24 +0200

    1

--- pages/includes/file_view.php
+++ pages/includes/file_view.php
@@ -1,7 +1,7 @@
 <?php
 if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM files INNER JOIN users ON users.badge_number=files.author WHERE dbid='$id'")) {
     $row = mysqli_fetch_assoc($result);
-    echo "<div id='files-content' align='center'>";
+    echo "<div id='files-content'>";
     echo "<div style='display: inline;'>";
     echo "<a class='button-danger' style='float:right;'>Törlés</a>";
     echo "<a href='index.php?p=files&t=edit&id=" . $id . "' class='button-norm' style='float:right;'>Módosítás</a>";
