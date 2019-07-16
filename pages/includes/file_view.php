cbfd398b4a99103c0632c02fad06d3216081fead
Áron Kovács <zipike02@gmail.com>
2019-07-16 21:46:13 +0200

    asd

--- pages/includes/file_view.php
+++ pages/includes/file_view.php
@@ -1,40 +1,40 @@
 <?php
 if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM files INNER JOIN users ON users.badge_number=files.author WHERE dbid='$id'")) {
     $row = mysqli_fetch_assoc($result);
-    echo "<div id='files-content'>";
+    echo "<div id='fileview_content'>";
     echo "<h1>Akták » " . $row['file_name'] . "</h1>";
     echo "<div style='display: inline;'>";
     echo "<a class='button-danger' style='float:right;'>Törlés</a>";
     echo "<a href='index.php?p=files&t=edit&id=" . $id . "' class='button-norm' style='float:right;'>Módosítás</a>";
     echo "</div>";
     echo "<br /><br />";
-    echo "<table id='files'>";
-        echo "<tr class='files-row'>";
-            echo "<td class='files-column'><b>Akta sorszáma</b></td>";
-            echo "<td class='files-column'>" . $row['file_name'] . "</td>";
+    echo "<table id='fileview_table'>";
+        echo "<tr>";
+            echo "<td><b>Akta sorszáma</b></td>";
+            echo "<td>" . $row['file_name'] . "</td>";
         echo "</tr>";
 
-        echo "<tr class='files-row'>";
-            echo "<td class='files-column'><b>Megnevezés</b></td>";
-            echo "<td class='files-column'>" . $row['title'] . "</td>";
+        echo "<tr>";
+            echo "<td><b>Megnevezés</b></td>";
+            echo "<td>" . $row['title'] . "</td>";
         echo "</tr>";
 
-        echo "<tr class='files-row'>";
-            echo "<td class='files-column'><b>Szerző</b></td>";
-            echo "<td class='files-column'><a href='index.php?p=users&t=view&id=" . $row['author'] . "'>" . RPName($row['playername']) . "</a></td>";
+        echo "<tr>";
+            echo "<td><b>Szerző</b></td>";
+            echo "<td><a href='index.php?p=users&t=view&id=" . $row['author'] . "'>" . RPName($row['playername']) . "</a></td>";
         echo "</tr>";
 
-        echo "<tr class='files-row'>";
-            echo "<td class='files-column'><b>Kelt</b></td>";
-            echo "<td class='files-column'>" . $row['timestamp'] . "</td>";
+        echo "<tr>";
+            echo "<td><b>Kelt</b></td>";
+            echo "<td>" . $row['timestamp'] . "</td>";
         echo "</tr>";
 
-        echo "<tr class='files-row'>";
-            echo "<td colspan='2' class='files-column'><b>Akta tartalma</b></td>";
+        echo "<tr>";
+            echo "<td colspan='2'><b>Akta tartalma</b></td>";
         echo "</tr>";
 
-        echo "<tr class='files-row'>";
-            echo "<td colspan='2' class='files-column'><br />" . $row['text'] . "<br /><br /></td>";
+        echo "<tr>";
+            echo "<td colspan='2'><br />" . $row['text'] . "<br /><br /></td>";
         echo "</tr>";
     echo "</table>";
     echo "</div>";
