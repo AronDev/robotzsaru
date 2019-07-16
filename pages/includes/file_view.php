2ec12a0ae6307ef007c381c46278698819fa112d
Áron Kovács <zipike02@gmail.com>
2019-07-16 18:11:57 +0200

    1

--- pages/includes/file_view.php
+++ pages/includes/file_view.php
@@ -1,11 +1,34 @@
 <?php
-if($result = mysqli_query($mysql_id, "SELECT * FROM files WHERE dbid='$dbid'")) {
+if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM files INNER JOIN users ON users.badge_number=files.author WHERE dbid='$dbid'")) {
     $row = mysqli_fetch_assoc($result);
     echo "<div id='files-content'>";
     echo "<table id='files'>";
         echo "<tr>";
             echo "<td><b>Akta sorszáma</b></td>";
             echo "<td>" . $row['file_name'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+            echo "<td><b>Megnevezés</b></td>";
+            echo "<td>" . $row['title'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+            echo "<td><b>Szerző</b></td>";
+            echo "<td>" . $row['playername'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+            echo "<td><b>Kelt</b></td>";
+            echo "<td>" . $row['timestamp'] . "</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+            echo "<td colspan='2'><b>Akta tartalma</b></td>";
+        echo "</tr>";
+
+        echo "<tr>";
+            echo "<td colspan='2'>" . $row['text'] . "</td>";
         echo "</tr>";
     echo "</table>";
     echo "</div>";
