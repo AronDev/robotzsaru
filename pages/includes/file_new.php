e481ed6af225912ed748697246e8486502e275b9
Áron Kovács <zipike02@gmail.com>
2019-07-16 22:42:57 +0200

    123

--- pages/includes/file_new.php
+++ pages/includes/file_new.php
@@ -1,46 +1,3 @@
-<?php
-if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM files INNER JOIN users ON users.badge_number=files.author WHERE dbid='$id'")) {
-    $row = mysqli_fetch_assoc($result);
-    echo "<div id='fileview_content'>";
-    echo "<h1>Akták » " . $row['file_name'] . "</h1>";
-    echo "<div style='display: inline;'>";
-    echo "<a class='button-danger' style='float:right;'>Törlés</a>";
-    echo "<a href='index.php?p=files&t=edit&id=" . $id . "' class='button-norm' style='float:right;'>Módosítás</a>";
-    echo "</div>";
-    echo "<br /><br />";
-    echo "<table id='fileview_table'>";
-        echo "<tr>";
-            echo "<td><b>Akta sorszáma</b></td>";
-            echo "<td>" . $row['file_name'] . "</td>";
-        echo "</tr>";
-
-        echo "<tr>";
-            echo "<td><b>Megnevezés</b></td>";
-            echo "<td>" . $row['title'] . "</td>";
-        echo "</tr>";
-
-        echo "<tr>";
-            echo "<td><b>Szerző</b></td>";
-            echo "<td><a href='index.php?p=users&t=view&id=" . $row['author'] . "'>" . RPName($row['playername']) . "</a></td>";
-        echo "</tr>";
-
-        echo "<tr>";
-            echo "<td><b>Kelt</b></td>";
-            echo "<td>" . $row['timestamp'] . "</td>";
-        echo "</tr>";
-
-        echo "<tr>";
-            echo "<td colspan='2' style='text-align:center;'><b>Akta tartalma</b></td>";
-        echo "</tr>";
-
-        echo "<tr>";
-            echo "<td colspan='2'>" . $row['text'] . "</td>";
-        echo "</tr>";
-    echo "</table>";
-    echo "</div>";
-} else echo "<h1>Hiba történt!</h1>";
-?>
-
 <div id='fileview_content'>
 <h1>Új akta</h1><br />
 <table id='fileview_table'>
@@ -61,7 +18,9 @@ if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM file
             <script>CKEDITOR.replace('editor1');CKEDITOR.config.width = '100%';</script>
         </td>
     </tr>
+    <tr>
+        <td colspan='2'><a class='button-primary submit-new-file'>Létrehozás</a></td>
+    </tr>
 </table>
-<a class='button-primary'>Létrehozás</a>
 </div>
 <script src='js/new_file.js'></script>
