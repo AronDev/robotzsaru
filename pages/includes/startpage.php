4fb0888c0835e8f509df71b1bfaa1f1c8dfa5ae0
Áron Kovács <zipike02@gmail.com>
2019-07-16 16:05:19 +0200

    1

--- pages/includes/startpage.php
+++ pages/includes/startpage.php
@@ -1,13 +1,13 @@
 <div id="startpage-widgets">
     <div onclick="window.location.href = 'index.php?p=activities';" class="startpage-widget">
         <div class='title'>
+            <i class="far fa-clipboard"></i>
             Akták
             <div class='description'>
                 Nyomozati akták
             </div>
         </div>
         <div class='other'>
-<i class="far fa-clipboard"></i>
         </div>
     </div>
 </div>
