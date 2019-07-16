4774fd398ab0b9071c5895cc601479fc6e642c7f
Áron Kovács <zipike02@gmail.com>
2019-07-16 16:31:59 +0200

    12

--- pages/includes/files.php
+++ pages/includes/files.php
@@ -7,20 +7,20 @@
         ÚJ AKTA TODO
         <table id="files">
             <tr>
-                <th>Akta</th>
-                <th>Időpont</th>
-                <th>Szerző</th>
-                <th>Megnevezés</th>
+                <th class='files-header'>Akta</th>
+                <th class='files-header'>Időpont</th>
+                <th class='files-header'>Szerző</th>
+                <th class='files-header'>Megnevezés</th>
             </tr>
             <?php
             $result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM files INNER JOIN users ON users.badge_number=files.author ORDER BY timestamp DESC");
             if(mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_assoc($result)) {
                     echo "<tr>";
-                        echo "<td>" . $row['file_name'] . "</td>";
-                        echo "<td>" . $row['timestamp'] . "</td>";
-                        echo "<td>" . $row['playername'] . "</td>";
-                        echo "<td>" . $row['title'] . "</td>";
+                        echo "<td class='files-row'>" . $row['file_name'] . "</td>";
+                        echo "<td class='files-row'>" . $row['timestamp'] . "</td>";
+                        echo "<td class='files-row'>" . $row['playername'] . "</td>";
+                        echo "<td class='files-row'>" . $row['title'] . "</td>";
                     echo "</tr>";
                 }
             } else echo "<tr><td colspan='4'>Nincsenek akták!</td></tr>";
