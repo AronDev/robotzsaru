66bced592899ba127e0576c64e0701af642ba994
Áron Kovács <zipike02@gmail.com>
2019-07-16 18:38:54 +0200

    a

--- pages/includes/files.php
+++ pages/includes/files.php
@@ -29,7 +29,7 @@
         <h1>Akták</h1><br />
         <div id="files-content">
             <input class='search' type='text' id='f_search' onkeyup='search_fileName()' placeholder='Keresés sorszám alapján' style='width: 20%'>
-            <a class='new-button' href='index.php?p=files&t=new' style='float:right;'>Új akta</a><br /><br />
+            <a class='button-primary' href='index.php?p=files&t=new' style='float:right;'>Új akta</a><br /><br />
             <table id="files">
                 <tr>
                     <th class='files-header'>Sorszám</th>
