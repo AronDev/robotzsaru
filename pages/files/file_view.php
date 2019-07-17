21e44f3e4a0edf73a059e8682025cbe076e9de11
Áron Kovács <zipike02@gmail.com>
2019-07-17 19:24:32 +0200

    213

--- pages/files/file_view.php
+++ pages/files/file_view.php
@@ -40,4 +40,3 @@ if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM file
     echo "</div>";
 } else echo "<h1>Hiba történt!</h1>";
 ?>
-<script src='js/file_view.js'></script>
