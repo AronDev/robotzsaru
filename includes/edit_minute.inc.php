b4ef67dd93d3b364b85cb1a794e67a3913381ac9
Áron Kovács <zipike02@gmail.com>
2019-07-19 00:23:44 +0200

    1

--- includes/edit_minute.inc.php
+++ includes/edit_minute.inc.php
@@ -5,8 +5,9 @@ require("connect.inc.php");
 $title = mysqli_real_escape_string($mysql_id, $_POST["title"]);
 $text = mysqli_real_escape_string($mysql_id, $_POST["text"]);
 $id = $_POST['id'];
+$bn = $_SESSION['badge_number'];
 
-$query = "UPDATE minutes SET title='" . $title . "', text='" . $text . "', edited_by='" . $_SESSION['badge_number'] . "', edited=CURRENT_TIMESTAMP() WHERE dbid='" . $id . "'";
+$query = "UPDATE minutes SET title='$title', text='$text', edited_by='$bn', edited=CURRENT_TIMESTAMP() WHERE dbid='$id'";
 
 if($result = mysqli_query($mysql_id, $query)) {
     echo "Sikeres szerkesztés!";
