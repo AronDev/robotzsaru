de1fd93dfe115fd4a5cc91408a7f0b7cc0d892f8
Áron Kovács <zipike02@gmail.com>
2019-07-19 00:26:42 +0200

    1

--- includes/edit_file.inc.php
+++ includes/edit_file.inc.php
@@ -6,8 +6,9 @@ $filename = mysqli_real_escape_string($mysql_id, $_POST["filename"]);
 $title = mysqli_real_escape_string($mysql_id, $_POST["title"]);
 $text = mysqli_real_escape_string($mysql_id, $_POST["text"]);
 $id = $_POST['id'];
+$bn = $_SESSION['badge_number'];
 
-if($result = mysqli_query($mysql_id, "UPDATE files SET file_name='$filename', title='$title', text='$text', edited=CURRENT_TIMESTAMP() WHERE dbid='$id'")) {
+if($result = mysqli_query($mysql_id, "UPDATE files SET file_name='$filename', title='$title', text='$text', edited_by='$bn', edited=CURRENT_TIMESTAMP() WHERE dbid='$id'")) {
     echo "Sikeres szerkesztés!";
 } else echo "Hiba történt!";
 
