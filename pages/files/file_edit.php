d109b0a8044a05b2b1297c23581c700a3b6e6a71
Áron Kovács <zipike02@gmail.com>
2019-07-19 08:13:37 +0200

    123

--- pages/files/file_edit.php
+++ pages/files/file_edit.php
@@ -2,42 +2,44 @@
 if(getUserRankPerm($_SESSION['badge_number']) >= 3 || $row['author'] == $_SESSION['badge_number']) {
     if($result = mysqli_query($mysql_id, "SELECT files.*, users.playername FROM files INNER JOIN users ON users.badge_number=files.author WHERE dbid='$id'")) {
         $row = mysqli_fetch_assoc($result);
-        echo "<div id='fileview_content'>";
-        echo "<h1>Akták » " . $row['file_name'] . " » Szerkesztés</h1>";
-        echo "<br /><br />";
-        echo "<table id='fileview_table'>";
-            echo "<tr>";
-                echo "<td><b>Akta sorszáma</b></td>";
-                echo "<td><input type='text' id='inputFileName' value='" . $row['file_name'] . "'></td>";
-            echo "</tr>";
+        if($row['archive'] == 0) {
+            echo "<div id='fileview_content'>";
+            echo "<h1>Akták » " . $row['file_name'] . " » Szerkesztés</h1>";
+            echo "<br /><br />";
+            echo "<table id='fileview_table'>";
+                echo "<tr>";
+                    echo "<td><b>Akta sorszáma</b></td>";
+                    echo "<td><input type='text' id='inputFileName' value='" . $row['file_name'] . "'></td>";
+                echo "</tr>";
 
-            echo "<tr>";
-                echo "<td><b>Megnevezés</b></td>";
-                echo "<td><input type='text' id='inputTitle' value='" . $row['title'] . "'></td>";
-            echo "</tr>";
+                echo "<tr>";
+                    echo "<td><b>Megnevezés</b></td>";
+                    echo "<td><input type='text' id='inputTitle' value='" . $row['title'] . "'></td>";
+                echo "</tr>";
 
-            echo "<tr>";
-                echo "<td colspan='2' style='text-align:center;'><b>Akta tartalma</b></td>";
-            echo "</tr>";
+                echo "<tr>";
+                    echo "<td colspan='2' style='text-align:center;'><b>Akta tartalma</b></td>";
+                echo "</tr>";
 
-            echo "<tr>";
-                echo "<td colspan='2'>";
-                    echo "<textarea name='editor1' id='editor1' rows='10' cols='80'></textarea><br />
-                    <script>
-                    CKEDITOR.replace('editor1');
-                    CKEDITOR.config.width = '100%';
-                    var editor1 = CKEDITOR.instances['editor1'];
-                    editor1.setData(`" . $row['text'] . "`);
-                    </script>";
-                echo "</td>";
-            echo "</tr>";
-            echo "<tr>";
-                echo "<td colspan='2'>";
-                    echo "<a class='button-primary submit-edit-file' id='$id'>Módosítás</a><span id='editfileInfo' style='margin-left: 10px;'>&nbsp</span>";
-                echo "</td>";
-            echo "</tr>";
-        echo "</table>";
-        echo "</div>";
+                echo "<tr>";
+                    echo "<td colspan='2'>";
+                        echo "<textarea name='editor1' id='editor1' rows='10' cols='80'></textarea><br />
+                        <script>
+                        CKEDITOR.replace('editor1');
+                        CKEDITOR.config.width = '100%';
+                        var editor1 = CKEDITOR.instances['editor1'];
+                        editor1.setData(`" . $row['text'] . "`);
+                        </script>";
+                    echo "</td>";
+                echo "</tr>";
+                echo "<tr>";
+                    echo "<td colspan='2'>";
+                        echo "<a class='button-primary submit-edit-file' id='$id'>Módosítás</a><span id='editfileInfo' style='margin-left: 10px;'>&nbsp</span>";
+                    echo "</td>";
+                echo "</tr>";
+            echo "</table>";
+            echo "</div>";
+        } else echo "<h1>Archivált aktát nem szerkeszthetsz!</h1>";
     } else echo "<h1>Hiba történt!</h1>";
 } else echo "<h1>Nem szerkeszthetsz akták amit nem te hoztál létre!</h1>";
 ?>
