5db0b215786ff3ce71f1a86dd583f1ab2239ae6a
Áron Kovács <zipike02@gmail.com>
2019-07-19 06:28:05 +0200

    1

--- pages/admin/adminstartpage.php
+++ pages/admin/adminstartpage.php
@@ -6,7 +6,7 @@
         $row = mysqli_fetch_assoc($result);
         echo "
             <div onclick='window.location.href = `index.php?p=admin&t=newusers`;' class='startpage-widget'>
-                Jegyzőkönyvek
+                Elfogadásra váró felhasználók
                 <i class='far fa-clock icon'></i>
                 <div class='description'>
                     Elfogadásra váró felhasználók: " . $row['pcs'] . "
@@ -18,7 +18,7 @@
     if(getUserRankPerm($_SESSION['badge_number']) >= 4) {
         echo "
             <div onclick='window.location.href = `index.php?p=admin&t=users`;' class='startpage-widget'>
-                Jegyzőkönyvek
+                Felhasználólista
                 <i class='far fa-user icon'></i>
                 <div class='description'>
                     Elfogadott felhasználók megtekintése és kezelése
