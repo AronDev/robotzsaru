4173679f14d489b9cb9077dbecf6c7a7c0fb2bda
Áron Kovács <zipike02@gmail.com>
2019-07-19 06:58:12 +0200

    213

--- includes/accept_user.inc.php
+++ includes/accept_user.inc.php
@@ -4,7 +4,7 @@ require("connect.inc.php");
 
 $id = $_POST['id'];
 
-if($result = mysqli_query($mysql_id, "UPDATE users SET activate='1' WHERE badge_number='$id'")) {
+if($result = mysqli_query($mysql_id, "UPDATE users SET active='1' WHERE badge_number='$id'")) {
 } else echo "Hiba történt!";
 
 // POST adatok törlése
