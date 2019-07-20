778df04902734bf8e9c835bda357f4054d599300
Áron Kovács <zipike02@gmail.com>
2019-07-20 21:32:01 +0200

    1

--- pages/register.php
+++ pages/register.php
@@ -20,12 +20,12 @@ include("../config.php");
             <h1>Robotzsaru</h1>
             <h4>Regisztrálás</h4>
             </div><br />
-            <input type="text" id="badgeNum" placeholder="Jelvényszám"><br /><br />
-            <input type="text" id="playerName" placeholder="Név"><br /><br />
-            <input type="password" id="password" placeholder="Jelszó"><br /><br />
-            <input type="password" id="password2" placeholder="Jelszó újra"><br /><br />
-            <button id="registerButton"><i class='fas fa-user-plus login-icon'></i>Regisztrálás</button><br /><br />
-            <button onclick="window.location.href = '../index.php';"><i class='fas fa-angle-left login-icon'></i>Vissza</button><br /><br />
+            <input type="text" id="badgeNum" class='input-primary' placeholder="Jelvényszám"><br /><br />
+            <input type="text" id="playerName" class='input-primary' placeholder="Név"><br /><br />
+            <input type="password" id="password" class='input-primary' placeholder="Jelszó"><br /><br />
+            <input type="password" id="password2" class='input-primary' placeholder="Jelszó újra"><br /><br />
+            <button id="registerButton" class='button-secondary'><i class='fas fa-user-plus login-icon'></i>Regisztrálás</button><br /><br />
+            <button onclick="window.location.href = '../index.php';" class='button-secondary'><i class='fas fa-angle-left login-icon'></i>Vissza</button><br /><br />
             <div id='registerInfo'>&nbsp</div><br />
         </div>
         <script src="../js/register.js"></script>
