586a267351f4e2f60f06199b89b503242f6abb3c
Áron Kovács <zipike02@gmail.com>
2019-07-27 06:18:11 +0200

    User search input and new user btn

--- pages/users/list.php
+++ pages/users/list.php
@@ -1,12 +1,37 @@
 <html>
     <head>
         <title><?php echo $WEBPAGE_NAME; ?> - Felhasználók</title>
+
+        <script>
+		function search_badgeNum() {
+		  // Declare variables
+		  var input, filter, table, tr, td, i;
+		  input = document.getElementById("badgeNum_search");
+		  filter = input.value.toUpperCase();
+		  table = document.getElementById("files");
+		  tr = table.getElementsByTagName("tr");
+
+		  // Loop through all table rows, and hide those who don't match the search query
+		  for (i = 0; i < tr.length; i++) {
+		    td = tr[i].getElementsByTagName("td")[0];
+		    if (td) {
+		      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+		        tr[i].style.display = "";
+		      } else {
+		        tr[i].style.display = "none";
+		      }
+            }
+		  }
+		}
+        </script>
     </head>
     <body>
         <?php
         if(getUserRankPerm($_SESSION['badge_number']) >= 4) {
             echo "<div id='files-content'>
                 <h1>Elbírálásra váró felhasználók</h1><br />
+                <input class='search input-primary' type='text' id='badgeNum_search' onkeyup='search_fileName()' placeholder='Keresés jelvényszám alapján' style='width: 20%'>
+                <a class='button-primary' href='index.php?p=users&t=new' style='float:right;'>Új felhasználó</a><br /><br />
                 <table id='files'>
                     <tr>
                         <th class='files-header'>Jelvényszám</th>
