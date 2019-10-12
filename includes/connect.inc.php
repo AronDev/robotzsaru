ae79278e7294005873f0354469e159ea5a3fa360
Áron Kovács <zipike02@gmail.com>
2019-10-12 21:48:51 +0200

    sql credentials moved to a separate file

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,8 +1,5 @@
 <?php
-define("HOSTNAME","localhost");
-define("USERNAME","robotzsaru");
-define("PASSWORD","r1o2b3o4t5z6s7a8r9u0");
-define("DATABASE","robotzsaru");
+include("credentials.inc.php");
 $mysql_id = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
 if (mysqli_connect_errno()) echo mysqli_connect_errno() . ": " . mysqli_connect_error();
 mysqli_query($mysql_id, "SET NAMES UTF8");
---
+++ includes/credentials.inc.php
@@ -0,0 +1,4 @@
+define("HOSTNAME","localhost");
+define("USERNAME","robotzsaru");
+define("PASSWORD","r1o2b3o4t5z6s7a8r9u0");
+define("DATABASE","robotzsaru");
